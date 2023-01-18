27a88e88a62a0ae7a562429a309e96feccc9a9c3
Alexandra <118150358+kssaxa@users.noreply.github.com>
2023-01-18 23:36:14 +1000

    Update Предметная область.cpp

--- Предметная область.cpp
+++ Предметная область.cpp
@@ -2,8 +2,8 @@
 
 #include<iostream>
 #include <string>
-class  Characteristics_plane {
-public:
+class  Plane {//Plane
+private:
 
     int Range_of_flight;//Дальность полета
     int Pass_mest;
@@ -16,26 +16,31 @@ public:
     std::string Crew;//Экипаж
 
     // в наследование идет тип самолета (пассажирский, военный, спасательный, грузовой далее к ним присоединяются другие классы)
-    
-
-};
-class Plane : public Characteristics_plane {
 public:
-    std::string type;//тип самолета (пассажирский,спасательный,военный)
+    virtual void Ridedal() {
+        if (Range_of_flight > 10000)
+            std::cout << "Годен для дальних перелетов\n";
+    }
+};
+class Safetyquipment : public Plane {//переименовать два в наследовании подвиды самолета
+private:
+    //std::string type;//тип самолета (пассажирский,спасательный,военный)
     bool Safety_equipment;//Спасательное оборудование
-    bool military_equipment;//Военное оборудование
+    //bool military_equipment;//Военное оборудование
+    bool education_crew;
+
 
     int Range_of_flight;
     int Pass_mest;
-    int Takeoff_weight; 
-    int Empty_weight; 
+    int Takeoff_weight;
+    int Empty_weight;
     int Wingspan;
     std::string model;
     std::string name;
     int Crew_count;
     std::string Crew;
 
-    Plane(int Range_of_flight, int Pass_mest, int Takeoff_weight, int Empty_weight,
+    Safetyquipment(int Range_of_flight, int Pass_mest, int Takeoff_weight, int Empty_weight,
         int Wingspan, std::string model, std::string name, int Crew_count, std::string Crew)
     {
         this->Range_of_flight = Range_of_flight;
@@ -49,7 +54,7 @@ public:
         this->Crew_count = Crew_count;
         this->Crew = Crew;
     }
-
+public:
     void print()
     {
 
@@ -64,39 +69,87 @@ public:
         std::cout << "Crew_count   : " << this->Crew_count << std::endl;
         std::cout << "Crew : " << this->Crew << std::endl;
 
-       
+
     }
- 
+
+    //полиморфный метод перевозка или работа делать что-то поразному метод get
+    virtual void Ridedal() override{
+        if (Range_of_flight > 10000)
+            std::cout << "Годен для дальних перелетов\n";
+    }
+
+
+
 
     std::string prov_Model()
     {
         std::string result;
-        if (Safety_equipment ==true )
+        if (Safety_equipment == true)
             return result = "Пригоден для ЧС";
-        if(military_equipment == true)
-            return result = "Пригоден для ВС";
+       /* if (military_equipment == true)
+            return result = "Пригоден для ВС";*/
     }
 
-    std::string prov_polet()
-    {
-        std::string result;
-        if (Range_of_flight >= 5000)
-            return result = "Пригоден для дальних перелетов";
-        else
-            return result = "Не пригоден для далних перелетов";
-    }
 
 };
 
+
+class Militryaquipment: public Plane{//переименовать два в наследовании подвиды самолета
+private:
+    //std::string type;//тип самолета (пассажирский,спасательный,военный)
+    bool military_equipment;//Спасательное оборудование
+    //bool military_equipment;//Военное оборудование
+    bool education_crew;
+
+
+    int Range_of_flight;
+    int Pass_mest;
+    int Takeoff_weight;
+    int Empty_weight;
+    int Wingspan;
+    std::string model;
+    std::string name;
+    int Crew_count;
+    std::string Crew;
+
+    Militryaquipment(int Range_of_flight, int Pass_mest, int Takeoff_weight, int Empty_weight,
+        int Wingspan, std::string model, std::string name, int Crew_count, std::string Crew)
+    {
+        this->Range_of_flight = Range_of_flight;
+        this->Pass_mest = Pass_mest;
+        this->Takeoff_weight = Takeoff_weight;
+        this->Empty_weight = Empty_weight;
+        this->Wingspan = Wingspan;
+
+        this->model = model;
+        this->name = name;
+        this->Crew_count = Crew_count;
+        this->Crew = Crew;
+    }
+public:
+    void print()
+    {
+        std::cout << "Range_of_flight       : " << this->Range_of_flight << std::endl;
+        std::cout << " Pass_mest       : " << this->Pass_mest << std::endl;
+        std::cout << "Takeoff_weight   : " << this->Takeoff_weight << std::endl;
+        std::cout << "Empty_weight : " << this->Empty_weight << std::endl;
+        std::cout << "Wingspan	     : " << this->Wingspan << std::endl;
+
+        std::cout << "model       : " << this->model << std::endl;
+        std::cout << " name       : " << this->name << std::endl;
+        std::cout << "Crew_count   : " << this->Crew_count << std::endl;
+        std::cout << "Crew : " << this->Crew << std::endl;
+    }
+
 int main()
 {
-  
-    
-    Plane RU(4, 8, 80, 87, "Safe", "A-854ds6", 1, "ivanov I.I.");
-    Plane GM(5, 8, 80, 87, "Safe", "A-854ds6", 1, "ivanov I.I.");
-    Plane FR(4, 8, 80, 87, "Safe", "A-854ds6", 1, "ivanov I.I.");
-    Plane CN(0, 8, 80, 87, "Safe", "A-854ds6", 1, "ivanov I.I.");
-      
+
+
+    Safetyquipment RU(4, 8/*, 80, 87, 25, "Safe", "A-854ds6", 1, "ivanov I.I."*/);
+    Safetyquipment GM(5, 8, 80, 87,25,"Safe", "A-854ds6", 1, "ivanov I.I.");
+    Safetyquipment FR(4, 8, 80, 87, 65,"Safe", "A-854ds6", 1, "ivanov I.I.");
+    Safetyquipment CN(0, 8, 80, 87, 88,"Safe", "A-854ds6", 1, "ivanov I.I.");
+
 
     RU.print();
     GM.print();
