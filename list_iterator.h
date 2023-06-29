6ad8c8a13047367d558ab429f1d85bbdf62671dc
Alexandra <118150358+kssaxa@users.noreply.github.com>
2023-06-29 15:57:46 +1000

    Add files via upload

---
+++ list_iterator.h
@@ -0,0 +1,56 @@
+#ifndef FORWARD_LIST_CONTAINER_FORWARD_LIST_ITERATOR_H
+#define FORWARD_LIST_CONTAINER_FORWARD_LIST_ITERATOR_H
+
+#include<iterator>
+#include <cassert>
+#include "list_node.h"
+namespace Container
+{
+    template<typename T>
+    class forward_list_iterator : public std::iterator<
+        std::forward_iterator_tag,
+        T, 
+        T*,
+        T&> {
+        friend class forward_list<T>;
+    public:
+        using value_type = T;
+        using reference = value_type&;
+        using const_reference = const reference;
+        using _forward_list_node = typename Container::forward_list_node<T>;
+        using _node_pointer = typename Container::forward_list_node<T>*;
+        using iterator = typename Container::forward_list_iterator<T>;
+        using const_iterator = const iterator;
+        reference operator* () const
+        {
+            return _pointee->_value;
+        }
+        iterator& operator++()
+        {
+            _pointee = _pointee->_next;
+            return *this;
+        }
+        iterator operator++(int)
+        {
+            iterator retval(_pointee);
+            (*this) += 3 ; //шаг
+            return retval;
+        }
+        bool operator == (iterator other) const
+        {
+            return _pointee == other._pointee;
+        }
+        bool operator!= (iterator other) const
+        {
+            return _pointee != other._pointee;
+        }
+        iterator* operator->() //опратор ->
+        {
+            return this;
+        }
+    private:
+        _node_pointer _pointee;
+        explicit forward_list_iterator(_node_pointer pointee) :_pointee(pointee) {};
+    };
+}
+#endif 
---
+++ list_node.h
@@ -0,0 +1,28 @@
+#ifndef FORWARD_LIST_CONTAINER_FORWARD_LIST_NODE_H
+#define FORWARD_LIST_CONTAINER_FORWARD_LIST_NODE_H
+
+# include<memory>
+namespace Container
+{
+    template <typename T> class forward_list;
+    template<typename T> class forward_list_iterator; 
+    template<typename T>
+    class forward_list_node
+    {
+        friend class forward_list_iterator<T>;
+        friend class forward_list<T>;
+    public:
+        using value_type = T;
+        using reference = value_type&;
+        using const_reference = const value_type&;
+        using _forward_list_node = typename Container::forward_list_node<T>;
+        using _node_pointer = typename Container::forward_list_node<T>*;
+
+    private:
+        value_type _value;
+        _node_pointer _next;
+        explicit forward_list_node(const_reference value = value_type{}, _node_pointer next = nullptr) : _value(value), _next(next) {};
+        ~forward_list_node() = default;
+    };
+}
+#endif
